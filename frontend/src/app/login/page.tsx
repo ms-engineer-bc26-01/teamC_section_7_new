680f36a0880e2fb5fce2888311363a4cde060873
ghimep622 <ghimep622@git.com>
2026-04-13 21:02:37 +0900

    feat:フォーム画面の実装

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,11 +2,16 @@
 
 export default function LoginPage() {
   return (
+<<<<<<< Updated upstream
     <main className="min-h-screen flex items-center justify-center bg-sky-50">
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold text-center mb-6">
           ログイン
         </h1>
+=======
+    // ① 全画面背景
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#E6F7FF] via-white to-[#FFE4F1] relative overflow-hidden  p-4">
+>>>>>>> Stashed changes
 
         <form
         className="space-y-4"
